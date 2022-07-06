c0cc8a5408573f23bedf5b13081e7f400bcf2c18
quandm <quandm@edsolabs.com>
2022-07-06 15:46:51 +0700

    rabbit mq demo

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+using System;
+using System.Text;
+
+namespace RabbitMQ.Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = "localhost";
+            var port = 5672;
+            var userName = "manhquan";
+            var password = "manhquan";
+            var factory = new ConnectionFactory
+            {
+                HostName = host,
+                Port = port,
+                UserName = userName,
+                Password = password,
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) => { 
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume("demo-queue", true, consumer);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+using RabbitMQ.Client;
+
+using System;
+using System.Text;
+
+namespace RabbitMQ.Producer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = "localhost";
+            var port = 5672;
+            var userName = "manhquan";
+            var password = "manhquan";
+            var factory = new ConnectionFactory
+            {
+                HostName = host,
+                Port = port,
+                UserName = userName,
+                Password = password,
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            var index = 1;
+            while (true)
+            {
+                Console.WriteLine("================"+ (index++).ToString() + "====================" );
+                Console.Write("Name: ");
+                var name = Console.ReadLine();
+                Console.Write("Message: ");
+                var message = Console.ReadLine();
+                var obj = new { Name = name, Message = message };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(obj));
+                channel.BasicPublish("", "demo-queue", null, body);
+            }
+        }
+    }
+}
